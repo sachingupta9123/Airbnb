bb2ae64a9c72ce9d39156399d37212e1a2b16f4d
Sachin Kumar <sachin912325@gmail.com>
2026-06-22 00:21:25 +0530

    mongo seeding

--- init/index.js
+++ init/index.js
@@ -1,16 +1,13 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+require("dotenv").config();
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust2";
-
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const MONGO_URL = process.env.MONGO_URL;
 
 async function main() {
   await mongoose.connect(MONGO_URL);
@@ -18,10 +15,19 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj)=>({...obj, owner: new mongoose.Types.ObjectId("69a6f380eaa97af918457da7")
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: new mongoose.Types.ObjectId("69a6f380eaa97af918457da7"),
   }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
 
-initDB();
+main()
+  .then(() => {
+    console.log("connected to DB");
+    return initDB();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
